082821cee5eeef14d0caa56407de0b492fee32dd
kkla320 <accounts_kkla@icloud.com>
2021-09-25 15:29:18 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GaugeProgressViewStyle
+
+A description of this package.
---
+++ Sources/GaugeProgressViewStyle/GaugeProgressViewStyle.swift
@@ -0,0 +1,6 @@
+public struct GaugeProgressViewStyle {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
