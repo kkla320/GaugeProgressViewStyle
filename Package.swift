91797ef9866429ff4be864cf1bbb0b64afd27253
kkla320 <accounts_kkla@icloud.com>
2022-03-20 12:29:55 +0100

    Added package for Trigonometry

--- Package.swift
+++ Package.swift
@@ -20,14 +20,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "ViewInspector", url: "https://github.com/nalexn/ViewInspector.git", from: "0.9.1")
+        .package(name: "ViewInspector", url: "https://github.com/nalexn/ViewInspector.git", from: "0.9.1"),
+        .package(url: "https://github.com/kkla320/Trigonometry", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GaugeProgressViewStyle",
-            dependencies: []),
+            dependencies: [
+                "Trigonometry"
+            ]),
         .testTarget(
             name: "GaugeProgressViewStyleTests",
             dependencies: [
