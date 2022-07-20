1e78404e8260e9074f6794270eef80500fdbdaf6
kkla320 <accounts_kkla@icloud.com>
2022-07-20 16:07:04 +0200

    Moved extension for SwiftUI Angle to gauge package

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "ViewInspector", url: "https://github.com/nalexn/ViewInspector.git", from: "0.9.1"),
-        .package(url: "https://github.com/kkla320/Trigonometry", from: "1.0.1"),
+        .package(url: "https://github.com/kkla320/Trigonometry", branch: "fix/#1_Angles"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
